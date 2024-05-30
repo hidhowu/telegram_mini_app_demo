4ae75275d81eb26c54aec4f44e8aac939fa9acd8
Josh Studios <hidhowugreat@gmail.com>
2024-05-30 18:45:14 +0100

    fixing api

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const botToken = '7330885374:AAHbCenotQ3SdZyErs4ViavZJp8wNqlb4hE'; // Replace with your actual bot token
+const botToken = '7496688147:AAHM52ute3rbaZuSP92Hb9ZXpH3L_3Ljqw4'; // Replace with your actual bot token
 // const botToken = '7401756498:AAEKhlbdzS3sgoYRGQkloiuU-tfKMbl-5bU'; // Replace with your actual bot token
 // const bot = new TelegramBot(botToken);
 const bot = new TelegramBot(botToken, { polling: true });
@@ -14,10 +14,10 @@ bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
 
     // Send a message with a web page preview
-    bot.sendMessage(chatId, 'This is a demo Telegram Web App', {
+    bot.sendMessage(chatId, 'This is a demo Telegram Web App.\n This app works like tapswap', {
         reply_markup: JSON.stringify({
             inline_keyboard: [[
-                { text: 'Try Demo App', url: 'https://t.me/Randywebbot/demoapp' }
+                { text: 'Try Demo App', url: 'https://t.me/demoswap_bot/app' }
             ]]
 
 
